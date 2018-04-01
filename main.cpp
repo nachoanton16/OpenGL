bcd9f5850dc3af19012734e19dd72714465c0469
Davidpa78 <davidpa7899@gmail.com>
2018-04-01 21:26:55 +0200

    Some Changes

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include <strings.h>
 
 
-#define M 0x10
-#define N 0x40
+#define M 50
+#define N 30
 #define MAX_VIDAS 3
 #define VIDAS 3
 #define POS_VACIA " "
@@ -18,6 +18,7 @@ void rellena(int matriz[M][N]){
     system("clear");
     int x, y;
 
+
     for(int n = 0; n<2; n++){
         x = rand() % M;
         y = rand() % N;
@@ -30,21 +31,21 @@ void rellena(int matriz[M][N]){
 void pintar(int matriz[M][N]){
     for(int f=0; f<M; f++){
         for(int c=0; c<N; c++)
-            printf("%s", matriz[f][c]? POS_LLENA);
+            printf("%s", matriz[f][c]? POS_LLENA : POS_VACIA);
     }
 
     } 
-    void main()
+
+    void teclas()
     {
         char a;
-        clrscr();
-        a=getche();
+        a=getch();
         if (a==13){ printf("Enter");}
         if (a==75){ printf("Flecha izquierda");}
         if (a==80){ printf("Flecha abajo");}
         if (a==77){ printf("Flecha derecha");}
         if (a==72){ printf("Flecha arriba");}
-        getch();
+        getc();
     }
 
 int main(){
@@ -54,14 +55,13 @@ int main(){
     int (*nueva)[M][N] = &pantalla[NUEVA];/*Se guardan los datos de la matriz NUEVA en *nueva*/
     int (*temporal)[M][N];
     bzero(pantalla[ACTUAL], sizeof(pantalla[ACTUAL]));
-
+    
     system("toilet --gay -fpagga Welcome to the CANION");
-
+    teclas();
 /*Continuar jugando*/
     while(VIDAS >= 1){
         system("clear");
         pintar(pantalla[ACTUAL]);
-
 
     }
 /*Fin del juego por acabarse el numero de vidas*/
